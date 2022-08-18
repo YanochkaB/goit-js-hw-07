0ea1e18ea1f2b1384a4a203ddc27ee39fd66c719
YanochkaB <yanabukhtij@gmail.com>
2022-08-18 11:18:05 +0300

    2 завдання готово

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery') //доступ до div у якому буде галерея
+const galleryList = createGaleryList(galleryItems) //створення нових div для картинок з доп. функції
+gallery.insertAdjacentHTML('beforeend', galleryList) // додавання новостворених об'єктів 
+
+function createGaleryList(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+        <a class="gallery__item" 
+            href="${original}">
+            <img class="gallery__image" 
+            src="${preview}"
+            alt="${description}" />
+        </a>
+        </div>`;
+        })
+        .join('')
+}
+
+const galleryLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
